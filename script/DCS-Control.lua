faf42470207fde1e871b619a4c985929788cebda
Ked57 <kedketh@gmail.com>
2019-04-09 21:10:04 +0200

    Working LUA script

--- script/DCS-Control.lua
+++ script/DCS-Control.lua
@@ -1,31 +1,53 @@
--- Thanks to Drex from Dynamic DCS for all the help on sockets, go check his server out
+local base = _G
 
-package.path = package.path .. ";.\\LuaSocket\\?.lua;"
+package.path = package.path .. ";.\\LuaSocket\\?.lua;" .. ".\\Scripts\\?.lua;" .. ".\\Scripts\\UI\\?.lua;"
 package.cpath = package.cpath .. ";.\\LuaSocket\\?.dll;"
 
-local socket = require("socket")
+local require = base.require
+local os = base.os
+local io = base.io
+local table = base.table
+local type = base.type
+
 local JSON = loadfile("Scripts\\JSON.lua")()
-local dcsc = {}
+local socket = require("socket")
+local net = require("net")
+local DCS = require("DCS")
+
+local dcsc = {
+	connection = nil,
+	logFile = io.open(lfs.writedir() .. [[Logs\DCS-Control.log]], "w")
+}
 
 dcsc.scope = "127.0.0.1"
 dcsc.port = 15488
 dcsc.DATA_TIMEOUT_SEC = 0.2
+dcsc.bound = false
 
 dcsc.JSON = JSON
-dcsc.isDev = true
-
 dcsc.tcp = socket.tcp()
 dcsc.tcp:settimeout(0)
 
--- Util functions
+module("DCS-Control")
+
+-- functions
+dcsc.functions = {}
+
+-- util
 
 function dcsc.log(message)
 	if message then
-		log.write(message)
+		net.log("[DCS-Control] LOG: " .. message)
 	end
 end
 
--- dcsc functions
+function dcsc.error(message)
+	if message then
+		net.log("[DCS-Control] ERROR: " .. message)
+	end
+end
+
+-- network
 
 function dcsc.bind()
 	local bound, error = dcsc.tcp:bind(dcsc.scope, dcsc.port)
@@ -39,24 +61,21 @@ function dcsc.bind()
 		dcsc.log("Could not start server: " .. error)
 		return
 	end
+	dcsc.bound = true
 	dcsc.log("Server started")
 end
+
 function dcsc.checkJSON(jsonstring, code)
 	if code == "encode" then
 		if type(dcsc.JSON:encode(jsonstring)) ~= "string" then
-			error("encode expects a string after function")
+			dcsc.error("encode expects a string after function")
 		end
 	end
 	if code == "decode" then
 		if type(jsonstring) ~= "string" then
-			error("decode expects string")
+			dcsc.error("decode expects string")
 		end
 	end
-end
-
-function dcsc.eventHandler.onEvent(event)
-	dcsc.log("event")
-	dcsc.log(event)
 end
 
 function dcsc.processRequest(request)
@@ -93,7 +112,7 @@ function dcsc.step()
 				dcsc.log(incMsg)
 				data = dcsc.processRequest(incMsg)
 			else
-				dcsc.log("Error: " .. error)
+				dcsc.error(error)
 			end
 		end
 		-- if there was no error, send it back to the dcsc.client
@@ -115,7 +134,7 @@ end
 
 function dcsc.send(dataPayload)
 	if not dcsc.client then
-		dcsc.log("Error: Connection lost")
+		dcsc.error("Connection lost")
 		return
 	end
 	local success, error = pcall(dcsc.checkJSON, dataPayload, "encode")
@@ -123,26 +142,25 @@ function dcsc.send(dataPayload)
 		local outMsg = dcsc.JSON:encode(dataPayload)
 		local bytes, status, lastbyte = dcsc.client:send(outMsg .. "\n")
 		if not bytes then
-			dcsc.log("Error: Connection lost")
+			dcsc.error("Connection lost")
 			dcsc.client = nil
 		end
 	else
-		dcsc.log("Error: " .. error)
+		dcsc.error(error)
 	end
 end
 
-dcsc.bind()
+function dcsc.onSimulationFrame()
+	if not dcsc.bound then
+		dcsc.log("Binding socket")
+		dcsc.bind()
+	end
+	local success, error = pcall(dcsc.step)
+	if not success then
+		dcsc.error(error)
+	end
+end
 
-timer.scheduleFunction(
-	function(arg, time)
-		local success, error = pcall(dcsc.step)
-		if not success then
-			dcsc.log("Error: " .. error)
-		end
-		return timer.getTime() + dcsc.DATA_TIMEOUT_SEC
-	end,
-	nil,
-	timer.getTime() + dcsc.DATA_TIMEOUT_SEC
-)
+DCS.setUserCallbacks(dcsc)
 
-dcsc.log("Started DCS-Control server")
+net.log("DCS-Control server started")
